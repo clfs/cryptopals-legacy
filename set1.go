6687466c9c5a350606e6cc0cd0cb212a62bd00ee
Calvin Figuereo-Supraner <mail@calvin.page>
2021-03-22 20:43:13 -0700

    Solve 3

--- set1.go
+++ set1.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// HexToBase64 converts hex strings to Base64 strings.
 func HexToBase64(s string) (string, error) {
 	h, err := hex.DecodeString(s)
 	if err != nil {
@@ -14,13 +15,53 @@ func HexToBase64(s string) (string, error) {
 	return base64.StdEncoding.EncodeToString(h), nil
 }
 
+// XORBytes XORs two byte slices. It fails if their lengths
+// are unequal.
 func XORBytes(a, b []byte) ([]byte, error) {
 	if len(a) != len(b) {
 		return nil, fmt.Errorf("unequal lengths")
 	}
-	c := make([]byte, len(a))
+	res := make([]byte, len(a))
 	for i := range a {
-		c[i] = a[i] ^ b[i]
+		res[i] = a[i] ^ b[i]
 	}
-	return c, nil
+	return res, nil
+}
+
+// XORByte XORs a byte slice with a single byte.
+func XORByte(a []byte, b byte) []byte {
+	res := make([]byte, len(a))
+	for i := range a {
+		res[i] = a[i] ^ b
+	}
+	return res
+}
+
+// SingleXORRecoverKey recovers the key from a single-byte-XOR
+// encrypted ciphertext.
+func SingleXORRecoverKey(ct []byte) (byte, error) {
+	if len(ct) == 0 {
+		return 0, fmt.Errorf("empty ciphertext")
+	}
+
+	freq := make(map[byte]int)
+	for _, b := range ct {
+		freq[b] += 1
+	}
+
+	var (
+		mode byte
+		best int
+	)
+
+	for b, count := range freq {
+		if count > best {
+			best = count
+			mode = b
+		}
+	}
+
+	// In Western languages, the most common byte
+	// is usually an ASCII space.
+	return mode ^ ' ', nil
 }
--- set1_test.go
+++ set1_test.go
@@ -10,19 +10,21 @@ func HelperHexDecode(tb testing.TB, s string) []byte {
 	tb.Helper()
 	h, err := hex.DecodeString(s)
 	if err != nil {
-		tb.Errorf("error: %v", err)
+		tb.Errorf("invalid string %s: err", s)
 	}
 	return h
 }
 
 func TestHexToBase64(t *testing.T) {
+	t.Parallel()
 	var (
 		s    = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 		want = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 	)
+
 	got, err := HexToBase64(s)
 	if err != nil {
-		t.Errorf("error: %v", err)
+		t.Error(err)
 	}
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
@@ -30,16 +32,35 @@ func TestHexToBase64(t *testing.T) {
 }
 
 func TestXORBytes(t *testing.T) {
+	t.Parallel()
 	var (
 		a    = HelperHexDecode(t, "1c0111001f010100061a024b53535009181c")
 		b    = HelperHexDecode(t, "686974207468652062756c6c277320657965")
 		want = HelperHexDecode(t, "746865206b696420646f6e277420706c6179")
 	)
+
 	got, err := XORBytes(a, b)
 	if err != nil {
-		t.Errorf("error: %v", err)
+		t.Error(err)
 	}
 	if !bytes.Equal(got, want) {
-		t.Errorf("got %v, want %v", got, want)
+		t.Errorf("got %x, want %x", got, want)
 	}
+}
+
+func TestSingleXORRecoverKey(t *testing.T) {
+	t.Parallel()
+	var (
+		ct        = HelperHexDecode(t, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+		want byte = 88
+	)
+
+	got, err := SingleXORRecoverKey(ct)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+	t.Logf("success: %s", XORByte(ct, got))
 }
